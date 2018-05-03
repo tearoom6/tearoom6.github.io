87139b8e47a74e543ec8b7ee8d57a0ae9333272d
Tomohiro Murota <tomohiro.murota@gmail.com>
2018-05-03 22:41:02 +0900

    Install React

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const title = 'tearoom6'
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('root')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'production',
+  entry: {
+    'index': [
+   	  path.resolve(__dirname, 'src/index.js'),
+    ],
+	},
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    publicPath: '/',
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
 }
