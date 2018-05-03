cef15c8c09e62900c25f57d49ec5192edf910042
Tomohiro Murota <tomohiro.murota@gmail.com>
2018-05-04 05:00:44 +0900

    Modify LinkedIn profile URL

--- src/SocialLinks.js
+++ src/SocialLinks.js
@@ -6,7 +6,7 @@ const SocialLinks = (props) => (
   <Container>
     <Image.Group size='mini'>
       <SocialLink href='https://github.com/tearoom6/' imgSrc='img/github.png' />
-      <SocialLink href='https://www.linkedin.com/in/tomohiro-m-604604155/' imgSrc='img/linkedin.png' />
+      <SocialLink href='https://www.linkedin.com/in/tearoom6/' imgSrc='img/linkedin.png' />
       <SocialLink href='https://twitter.com/tearoom6/' imgSrc='img/twitter.png' />
       <SocialLink href='https://qiita.com/tearoom6/' imgSrc='img/qiita.png' />
     </Image.Group>
